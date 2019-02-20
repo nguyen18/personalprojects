22659151229f4ed553c9c9f7f0a8bf4c6fb71de4
nguyen18 <31105362+nguyen18@users.noreply.github.com>
2019-02-20 01:48:41 -0800

    finished demo class

--- Noodles/src/Demo.java
+++ Noodles/src/Demo.java
@@ -1,9 +1,67 @@
+import java.util.*;
 
 public class Demo {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		
+		Noodles garlicNoodles = new Noodles(); 
+		Noodles ramen = new Noodles();
+		
+		garlicNoodles.setOrder("garlic noodles");
+		garlicNoodles.setSoupType("no soup");
+		garlicNoodles.setPrice(9.50);
+		garlicNoodles.setSpicy(false);
+		
+		ramen.setOrder("ramen");
+		ramen.setSoupType("rich miso soup");
+		ramen.setPrice(10.50);
+		ramen.setSpicy(true);
+		
+		Noodles alfredoPasta = new Noodles("alfredo pasta", "no soup", 12.20, false);
+		Noodles sphegetti = new Noodles("sphegetti", "no soup", 12.0, false);
+		
+		ArrayList<Noodles> myDishes = new ArrayList<Noodles>();
+		
+		myDishes.add(garlicNoodles);
+		myDishes.add(ramen);
+		myDishes.add(alfredoPasta);
+		myDishes.add(sphegetti);
+		
+		System.out.println("printing the fields on each instance of Noodles object");
+		System.out.println();
+		
+		for(int i = 0; i < myDishes.size(); i++) {
+			myDishes.get(i).displayData();
+			System.out.println();
+		}
+		
+		System.out.println("Testing the rest of the Noodles class methods below");
+		System.out.println("-----------------------------------------");
+		
+		Scanner scan = new Scanner(System.in);
+		Noodles userNoodles = new Noodles();
+		
+		System.out.println("Hello welcome to the noodle shop!");
+		System.out.println();
+		
+		String runAgain;
+		do {
+		System.out.println("Here is the menu:");
+		System.out.println("- pho \n- ramen \n- garlic noodles \n- custom \n**please spell correctly!**");
+		String desiredDish = scan.nextLine();
+		
+		userNoodles.makeOrder(desiredDish);
+		userNoodles.serveOrder();
+		
+		System.out.println();
+		System.out.println("Would you like to order again?");
+		runAgain = scan.nextLine();
+		
+		} while (runAgain.toLowerCase().equals("yes"));
+		
+		System.out.println();
+		System.out.println("Goodbye!");
+		 
 	}
 
 }
--- Noodles/src/Noodles.java
+++ Noodles/src/Noodles.java
@@ -3,17 +3,23 @@ import java.util.*;
 public class Noodles {
 
 	private String soupType;
+	private String order;
 	private double price;
 	private boolean spicy;
 	
 	public Noodles() { //default constructor
-		this("beef broth", 100, false);
+		this("Chicken Noodle Soup", "chicken soup", 8.50, false);
 	}
 	
-	public Noodles(String soupType, double price, boolean spicy) {
+	public Noodles(String order, String soupType, double price, boolean spicy) {
+		this.order = order;
 		this.soupType = soupType;
 		this.price = price;
 		this.spicy = spicy;
+	}
+	
+	public String getOrder() {
+		return order;
 	}
 	
 	public String getSoupType() {
@@ -26,6 +32,10 @@ public class Noodles {
 	
 	public boolean getSpicy() {
 		return spicy;
+	}
+	
+	public void setOrder(String order) {
+		this.order = order;
 	}
 	
 	public void setSoupType(String soupType) {
@@ -41,51 +51,58 @@ public class Noodles {
 	}
 	
 	public void displayData() {
+		System.out.println("order: " + order);
 		System.out.println("soup: " + soupType);
 		System.out.println("price:" + price);
 		System.out.println("spicy: " + spicy);
 	}
 	
 	public void makeOrder(String menuItem) {
+		
+		Scanner userInput = new Scanner(System.in);
+		
 		if(menuItem.toLowerCase().equals("pho")) {
-			soupType = "beef broth";
+			order = "pho";
+			soupType = "beef broth soup";
 			price = 8.50;
 		} 
 		else if(menuItem.toLowerCase().equals("garlic noodles")) {
-			soupType = "no";
+			order = "garlic noodles";
+			soupType = "no soup";
 			price = 9.50;
 		} 
 		else if(menuItem.toLowerCase().equals("ramen")) {
-			soupType = "rich miso";
+			order = "ramen";
+			soupType = "rich miso soup";
 			price = 10.50;
 		} 
 		else {
-			Scanner userInput = new Scanner(System.in);
+			order = "custom";
 			
-			System.out.println("What kind of soup would you like? (type no for no soup)");
+			System.out.println("What kind of soup would you like? (type no soup for no soup)");
 			soupType = userInput.nextLine();
-			
-			System.out.println("Do you like spicy?");
-			String spicyAnswer = userInput.nextLine();
-			
-				if(spicyAnswer.toLowerCase().equals("yes")) {
-					spicy = true;
-				} else {
-					spicy = false;
-				}
+
 		}
 		
+		System.out.println("Do you like spicy?");
+		String spicyAnswer = userInput.nextLine();
+		
+			if(spicyAnswer.toLowerCase().equals("yes")) {
+				spicy = true;
+			} else {
+				spicy = false;
+			}
 	}
 	
 	public void serveOrder() {
-		System.out.println("Your order is: ");
+		System.out.println("Your " + order + " order is: ");
 		System.out.println();
 		
 		if(spicy == true) {
-			soupType = "spicy " + soupType;
+			soupType = "spicy, " + soupType;
 		}
 		
-		System.out.println(soupType + " soup. This will cost " + price);
+		System.out.println(soupType + " and some yummy noodles. This will cost " + price);
 		
 	}
 	
